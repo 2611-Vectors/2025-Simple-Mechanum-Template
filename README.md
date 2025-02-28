bdf9cb25d6c65be509ed00bf21076f430625defe
LarryMoe11 <regwes0310a@gmail.com>
2025-02-28 18:32:22 -0500

    Updated README

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 This is a template code for mechanum robots in 2025.
+
+Please note that WPILib has a better (in some ways) implimentation, [WPILib Docs](https://docs.wpilib.org/en/stable/docs/software/hardware-apis/motors/wpi-drive-classes.html)
--- src/main/java/frc/robot/subsystems/drivetrain/SparkFlexDrivetrain.java
+++ src/main/java/frc/robot/subsystems/drivetrain/SparkFlexDrivetrain.java
@@ -6,7 +6,6 @@ package frc.robot.subsystems.drivetrain;
 
 import com.revrobotics.spark.SparkFlex;
 import java.util.function.Supplier;
-
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
